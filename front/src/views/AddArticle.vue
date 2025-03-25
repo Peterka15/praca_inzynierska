645b7816d811f54bcd5490c96ba16f63be014794
Peterka <pklosek@onet.pl>
2025-03-25 17:31:26 +0100

    Add article remix;

--- front/src/views/AddArticle.vue
+++ front/src/views/AddArticle.vue
@@ -1,58 +1,77 @@
 <template>
-  <div v-if="loggedIn">
-    <navbar/>
-    <div class="m_boxcenter m_boxcenterp shadow">
-      <h3 class="m_boxfont">NOWY ARTYKUŁ</h3>
-    </div>
-    <div class="workspace">
-      <b-container>
-        <b-row>
-          <b-col cols="6">
-            <div style="background: white">
-              <vue-editor v-model="content"/>
-            </div>
-          </b-col>
-          <b-col cols="6">
-            <div class="name_form">
-              <input class="name_formp" type="text" id="fname" name="fname" placeholder="Nazwa artykułu"
-                     v-model="title"><br>
-            </div>
-            <div class="name_form">
-              <input class="tags" type="text" placeholder="Tagi (oddziel przecinkami)" v-model="raw_tags"><br>
-            </div>
-            <div class="addphotos">
-              <b-button variant="primary" v-b-modal.photo-modal>Dodaj zdjęcia +</b-button>
-              <b-alert v-if="validationError" variant="danger" show dismissible @dismissed="validationError = null">
-                {{ validationError }}
-              </b-alert>
-              <b-modal id="photo-modal">
-                <h3>
-                  Wybierz zdjęcia do dodania
-                </h3>
-                <b-form-file v-model="photos" class="mt-3" plain></b-form-file>
-              </b-modal>
-            </div>
-            <div class="add_article_button">
-              <b-button variant="primary" style="margin-bottom: 10px" @click="saveArticle()">Dodaj Artykuł</b-button>
-              <b-button style="background: red">Usuń artykuł</b-button>
-            </div>
+  <b-card
+      title="Dodaj artykuł"
+  >
+    <b-form @submit.prevent="saveArticle">
+      <b-form-group
+          label="Tytuł"
+          label-for="input-form-input-title"
+          description="Wprowadź tytuł artykułu."
+          class="mt-4"
+      >
+        <b-form-input
+            id="input-form-input-title"
+            v-model="title"
+            type="text"
+            placeholder="Tytuł"
+            required
+        ></b-form-input>
+      </b-form-group>
 
-          </b-col>
-        </b-row>
-      </b-container>
-    </div>
-  </div>
-  <h1 v-else style="background-color: white; color: black;">Dostęp nielegalny</h1>
+      <b-form-group
+          label="Treść"
+          label-for="input-form-input-tags"
+          description="Wprowadź treść artykułu."
+          class="mt-4"
+      >
+        <vue-editor v-model="content" :editor-toolbar="customToolbar" id="input-form-editor"/>
+      </b-form-group>
+
+      <b-form-group
+          label="Tagi"
+          label-for="input-form-input-tags"
+          description="Wprowadź tagi oddzielone przecinkami."
+          class="mt-4"
+      >
+        <b-form-input
+            id="input-form-input-tags"
+            v-model="raw_tags"
+            type="text"
+            placeholder="Pożar, Akcja, OSP, Latarnia"
+            required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group
+          label="Zdjęcia"
+          label-for="input-form-input-photos"
+          description="Dodaj zdjęcia do artykułu."
+          class="mt-4"
+      >
+        <b-form-file v-model="photos" class="mt-3" multiple accept="image/*"></b-form-file>
+
+        <div class="mt-3 d-flex flex-wrap" style="gap: 1em">
+          <div v-for="(img, index) in photoPreviews" :key="index">
+            <img :src="img" alt="Podgląd obrazka" style="height: 100px; object-fit: cover"/>
+          </div>
+        </div>
+      </b-form-group>
+
+      <b-alert show v-if="validationError" variant="danger" class="mt-3 text-center">{{ validationError }}</b-alert>
+
+      <div class="d-flex justify-content-end">
+        <b-button variant="primary" type="submit" class="d-inline">Dodaj artykuł</b-button>
+      </div>
+    </b-form>
+  </b-card>
 </template>
 
 <script>
-import {VueEditor} from "vue2-editor";
+import {VueEditor} from 'vue2-editor';
 import Navbar from '/src/components/Navbar.vue';
-import Article from "@/Model/Article";
-import Tag from "@/Model/Tag";
-// import Bridge from "@/api/Bridge";
-import dataStorage from "@/Data/DataStorageInstance";
-import auth from "@/Model/AuthInstance";
+import Article from '@/Model/Article';
+import Tag from '@/Model/Tag';
+import dataStorage from '@/Data/DataStorageInstance';
 
 export default {
   name: 'Mainpage',
@@ -62,8 +81,17 @@ export default {
     return {
       title: '',
       content: '',
-      raw_tags: "",
+      raw_tags: '',
+      photos: null,
+      photoPreviews: [],
       validationError: null,
+      customToolbar: [
+        ['bold', 'italic', 'underline', 'strike'],
+        [{list: 'ordered'}, {list: 'bullet'}],
+        ['code-block'],
+        [{header: [1, 2, 3, 4, 5, 6]}],
+        ['link']
+      ]
     }
   },
 
@@ -79,10 +107,13 @@ export default {
         return;
       }
 
-      const tags = this.raw_tags.split(',').map(tagText => new Tag(tagText.trim()));
-      const article = new Article(this.title, this.content, tags);
+      const tags = this.raw_tags
+          .trim()
+          .split(',')
+          .filter(tagText => tagText.length)
+          .map(tagText => new Tag(tagText.trim()));
 
-     // Bridge.setBearerToken('34|f0zn0BWpVKZimKURjquUbvWgeVhGTAH41RnGjEG3');
+      const article = new Article(this.title, this.content, tags);
 
       article.save().then((article) => {
         console.log('Udało się!');
@@ -98,10 +129,18 @@ export default {
       return tagsArray.length > 1;
     },
   },
-  computed: {
-    loggedIn: function () {
-      return auth.loggedIn
-    },
+
+  watch: {
+    photos(newFiles) {
+      this.photoPreviews = [];
+      if (!newFiles) return;
+
+      Array.from(newFiles).forEach(file => {
+        const reader = new FileReader();
+        reader.onload = e => this.photoPreviews.push(e.target.result);
+        reader.readAsDataURL(file);
+      });
+    }
   },
 }
 </script>
