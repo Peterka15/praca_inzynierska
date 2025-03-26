9c6cc18ea8cedfca4f96827971977a801a36034d
Peterka <pklosek@onet.pl>
2025-03-26 17:03:37 +0100

    Universal search on mainpage;

--- front/src/components/Sidebar.vue
+++ front/src/components/Sidebar.vue
@@ -3,79 +3,51 @@
     <h1 class="logo">
       <router-link to="/">
         <span v-if="!auth.loggedIn">POWIATOWY SYSTEM OSP</span>
-        <span v-else>{{auth.user.unit.name}}</span>
+        <span v-else>{{ auth.user.unit.name }}</span>
       </router-link>
     </h1>
-    <div class="nav-wrap">
-      <nav class="main-nav" role="navigation">
-        <ul class="list-hover-slide">
-          <li>
-            <a @click="showSearchForm = true" style="color: #DFDBD9">Wyszukaj</a>
-            <div v-if="showSearchForm">
-              <form @submit.prevent="submitSearch">
-                <a> <input type="text" v-model="searchPhrase" placeholder="Nazwa artykułu..."></a>
-                <b-button type="submit" style="margin: 7px">Szukaj</b-button>
-              </form>
-            </div>
-          </li>
-          <li v-if="auth.loggedIn && (auth.user.role.isAdmin() || auth.user.role.isModerator())">
-            <router-link to="/addarticle">Dodaj artykuł</router-link>
-          </li>
-          <li v-if="auth.loggedIn">
-            <router-link to="/materials">Materiały szkoleniowe</router-link>
-          </li>
-          <li>
-            <a @click="toggleTagList" style="cursor: pointer">Tagi</a>
-            <div v-if="showTagList">
-              <form @submit.prevent="searchByTag('tag')">
-                <input type="text" v-model="tagSearchPhrase" placeholder="Szukaj po tagu...">
-                <b-button type="submit" style="margin: 7px">Szukaj</b-button>
-              </form>
-              <ul>
-                <li v-for="tag in tags" :key="tag" @click="searchByTag(tag)">
-                  <a href="#">{{ tag }}</a>
-                </li>
-              </ul>
-            </div>
-          </li>
-        </ul>
-      </nav>
-    </div>
+
+    <b-card title="Szukaj..." sub-title="Wyszukaj artykuły lub tagi">
+      <b-form-input
+          v-model="inputValue"
+          type="text"
+          placeholder="Akcja gaśnicza"
+      ></b-form-input>
+    </b-card>
   </div>
 </template>
 
 <script>
 import auth from '@/Model/AuthInstance';
+import dataStorage from '@/Data/DataStorageInstance';
 
 export default {
   name: 'Sidebar',
 
-  data(){
-    return{
-      showSearchForm: false,
-      searchPhrase: '',
-      showTagList: false,
-      tags: ['Pożar', 'Sprzęt', 'Załoga'], // Replace with your actual tags or fetch dynamically
-      tagSearchPhrase: '',
+  model: {
+    prop: 'modelValue',
+    event: 'update:modelValue'
+  },
+  
+  data() {
+    return {
+      tags: dataStorage.tags.data,
       auth
     }
   },
-  // TODO: Dodać wyszukiwanie nazw
-  methods: {
-    submitSearch() {
-      this.$emit("submitSearch", this.searchPhrase);
-      console.log('Wyszukiwanie:', this.searchPhrase);
 
-      // this.searchQuery = '';
-      // this.showSearchForm = false;
-    },
-    toggleTagList() {
-      this.showTagList = !this.showTagList;
-    },
-    searchByTag(tag) {
-      this.$emit("submitSearchTag", tag);
-      console.log('Wyszukiwanie po tagu:', tag);
-      this.showTagList = false;
+  props: {
+    modelValue: String
+  },
+
+  computed: {
+    inputValue: {
+      get() {
+        return this.modelValue
+      },
+      set(value) {
+        this.$emit('update:modelValue', value)
+      }
     }
   }
 }
--- front/src/views/Mainpage.vue
+++ front/src/views/Mainpage.vue
@@ -2,7 +2,7 @@
   <b-container class="mt-4">
     <b-row>
       <b-col>
-        <Sidebar @submitSearch="handleSubmitSearch" @submitSearchTag="handleSubmitSearchTag"></Sidebar>
+        <Sidebar v-model="searchPhrase"/>
       </b-col>
       <b-col cols="9">
         <Feed v-for="article in filteredArticles" :key="article.id" :article="article"></Feed>
@@ -15,7 +15,7 @@
 import Navbar from '/src/components/Navbar.vue';
 import Feed from '/src/components/Feed.vue';
 import Sidebar from '/src/components/Sidebar.vue';
-import dataStorageInstance from "@/Data/DataStorageInstance";
+import dataStorageInstance from '@/Data/DataStorageInstance';
 
 export default {
   name: 'Mainpage',
@@ -57,33 +57,20 @@ export default {
     },
 
     filteredArticles() {
+      if (this.searchPhrase === '') {
+        return this.articles;
+      }
+      
       const searchTermLower = this.searchPhrase.toLowerCase();
 
-      const uniqueArticles = [];
-      const seenIds = new Set();
+      return this.articles.filter((article) => {
+        console.log(article);
+        const isTitleMatch = article.title.toLowerCase().includes(searchTermLower);
+        const isTagMatch = article.tags.filter((tag) => tag.name.toLowerCase().includes(searchTermLower)).length > 0;
 
-      this.articles.forEach((article) => {
-        const articleTitleLower = article.title.toLowerCase();
-        const isTitleMatch = this.searchPhrase === '' || articleTitleLower.includes(searchTermLower);
-        const isTagMatch = this.searchTag === '' || article.tags.filter((tag) => tag.name === this.searchTag).length > 0;
-
-        if (isTitleMatch && isTagMatch && !seenIds.has(article.id)) {
-          uniqueArticles.push(article);
-          seenIds.add(article.id);
-        }
+        return isTitleMatch || isTagMatch;
       });
-
-      return uniqueArticles;
     },
-  },
-
-  methods: {
-    handleSubmitSearch(searchPhrase) {
-      this.searchPhrase = searchPhrase;
-    },
-    handleSubmitSearchTag(searchTag) {
-      this.searchTag = searchTag;
-    },
-  },
+  }
 };
 </script>
